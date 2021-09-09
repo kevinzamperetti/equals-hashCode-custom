d66ad3f2fe987be59bf6c1d9a235092d99c4f03b
Kevin Zamperetti Schepke <kevin.zamperetti92@gmail.com>
2021-09-09 00:11:44 -0300

    Equal and HashCode customization example.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace course {
+    class Program {
+        static void Main(string[] args) {
+            
+            Client a = new Client { Name = "Kevin", Email = "kevin@gmail.com"};
+            Client b = new Client { Name = "John", Email = "john@gmail.com"};
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
